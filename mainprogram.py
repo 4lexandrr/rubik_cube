a1f33f9aca438637aa87476dee547c431b6dd5ff
Aleksandr Pustovalov <4lexandrrr@mail.ru>
2023-05-29 21:50:38 +0300

    add find small cube and green color

--- mainprogram.py
+++ mainprogram.py
@@ -2,26 +2,32 @@ import cv2
 import numpy as np
 
 
-def main(camera=0):
-    cap = cv2.VideoCapture(camera)
+def find_color(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([40, 150, 20])
+    upper = np.array([100, 255, 255])
+    mask = cv2.inRange(image, lower, upper)
 
-    while True:
-        ret, frame = cap.read()
-        flipped_frame = cv2.flip(frame, 1)
-        image, grid = detect_cube(flipped_frame)
-
-        cv2.imshow('Frame', image)
-        print(grid)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv2.destroyAllWindows()
+    cv2.imshow('mask', mask)
+    return mask
 
 
-def find_color()
-    pass
+def draw_cube(image, colors):
+
+    for row in range(3):
+        for col in range(3):
+            x1 = col * 30
+            y1 = row * 30
+            x2 = x1 + 30
+            y2 = y1 + 30
+
+            if colors[row][col] == 255:
+                color = (0, 0, 255)
+            else:
+                color =  (255, 255, 255)
+
+            cv2.rectangle(image, (x1, y1), (x2, y2), color, -1)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 0), 1)
 
 
 def detect_cube(image):
@@ -32,7 +38,7 @@ def detect_cube(image):
 
     contours, _ = cv2.findContours(thresh,cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 
-    grid_cube = []
+    colors = []
 
     for contour in contours:
         # Площадь одного кубика на стороне
@@ -42,21 +48,34 @@ def detect_cube(image):
             # Периметр одного кубика на стороне
             pm_one_cube = cv2.arcLength(contour, True)
 
-            if cv2.norm(pm_one_cube**2/16 - sq_one_cube) < 200:
+            if cv2.norm(pm_one_cube**2/16 - sq_one_cube) < 150:
                 x, y, w, h = cv2.boundingRect(contour)
                 x, y, w, h = x + 5, y + 5, w - 10, h - 10
-                cube = np.array(cv2.mean(image[y:y+h,x:x+w])).astype(int)[:-1]
+                cube = np.array(cv2.mean(image[y:y + h, x:x + w])).astype(int)[:-1]
                 image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
                 val = (50 * y) + (10 * x)
-                cube = np.append(cube, val)
-                grid_cube.append(cube) 
-
-    if len(grid_cube) > 0:
-        grid_cube = np.asarray(grid_cube)
-        grid_cube = grid_cube[grid_cube[:, -1].argsort()]
-    return image, grid_cube
-
+                colors.append(cube)
+        
+    return image, colors
 
 
 if __name__ == '__main__':
-    main(camera=0)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        flipped_frame = cv2.flip(frame, 1)
+        image, grid = detect_cube(flipped_frame)
+        mask = find_color(image)
+        draw_cube(image, mask)
+
+        cv2.imshow('Frame', image)
+        
+        if cv2.waitKey(1) & 0xFF == ord('l'):
+            print(grid)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
